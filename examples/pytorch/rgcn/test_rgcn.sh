553f487c03703521f282b895516a77748c46fc40
Dominique LaSalle <dlasalle@nvidia.com>
2020-12-07 16:24:41 -0800

    Add test script

---
+++ examples/pytorch/rgcn/test_rgcn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+GPUS="${1}"
+
+NSYS="nsys profile -o /tmp/rgcn_base -f true --trace=nvtx,cuda"
+
+DGL_USE_CUDA_MEMORY_POOL=1 ${NSYS} python3 entity_classify_mp.py -d ogbn-mag \
+  --testing --fanout='25,30' --batch-size 512 --n-hidden 64 --lr 0.01 \
+  --num-worker 0 --eval-batch-size 8 --low-mem --gpu ${GPUS} --dropout 0.5 \
+  --use-self-loop --n-bases 2 --n-epochs 3 --mix-cpu-gpu --node-feats \
+  --sparse-embedding
