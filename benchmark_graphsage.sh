8a12af42ed37e65a85f4577aa6189bdea2d6ed3f
Ubuntu <ubuntu@ip-172-31-10-162.ec2.internal>
2020-12-17 10:34:00 +0000

    Set default to papers100m

--- benchmark_graphsage.sh
+++ benchmark_graphsage.sh
@@ -7,7 +7,7 @@ pushd examples/pytorch/graphsage/
 
 DGL_USE_CUDA_MEMORY_POOL=1 OMP_NUM_THREADS=6 python3 train_sampling_multi_gpu.py --gpu="${GPUS}" \
 	--num-epochs "${NUM_EPOCHS}" \
-	--dataset ogbn-products \
+	--dataset ogbn-papers100M \
 	--num-workers=6 \
 	--num-hidden 256 \
 	--fan-out=15,10,5 --num-layers=3 \
